a554dc08eacaf47d3a971c06ef811288a9c39957
zuericee <celine.schwarz@uzh.ch>
2025-11-11 13:15:30 +0100

    make it multipage

--- app/app.py
+++ app/app.py
@@ -1,2 +1,14 @@
 import streamlit as st
-st.title("Housing Affordability in Zurich")
+import pandas as pd
+
+st.set_page_config(page_title="Can you afford to live in Zurich?", layout="wide")
+
+homepage = st.Page("pages/homepage.py", title="Homepage")
+
+data_page = st.Page("pages/data_overview.py")
+
+user_pages = [homepage, data_page]
+
+pg = st.navigation(user_pages, position="sidebar", expanded=True)
+
+pg.run()
